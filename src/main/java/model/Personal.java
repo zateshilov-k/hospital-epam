42455f757fcf5335c064ccd9e563701648a01316
ColdAsIcee <Vladislav_Rospuskov@epam.com>
2018-12-25 17:37:22 +0300

    writing personal model

--- src/main/java/model/Personal.java
+++ src/main/java/model/Personal.java
@@ -5,4 +5,28 @@ public class Personal {
     private int id;
     private String name;
     private String email;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
