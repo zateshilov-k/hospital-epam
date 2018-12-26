168ef6875ca4506bbc8574e8afcb2fe0f69bab42
Alexey <taler2002@gmail.com>
2018-12-26 12:03:53 +0300

    проверка связи

--- src/main/java/controllers/Login.java
+++ src/main/java/controllers/Login.java
@@ -4,13 +4,15 @@ import services.LoginValidate;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet("/login")
+@WebServlet("/")
 public class Login extends HttpServlet {
     private String password;
     private String login;
@@ -18,12 +20,18 @@ public class Login extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        System.out.println("doGet method");
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        System.out.println("doPost method");
         response.setContentType("text/html");
         // получаем параметр login
         login = request.getParameter("login").trim();
-
         // получаем параметр password
         password = request.getParameter("password");
+        System.out.println(login + " " + password);
 
         LoginValidate loginValidate = new LoginValidate();
         boolean result = loginValidate.authenticate(login, password);
@@ -38,8 +46,9 @@ public class Login extends HttpServlet {
         }
     }
 
-    @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
-    }
+//    @Override
+//    public void service(ServletRequest req, ServletResponse res) throws ServletException, IOException {
+//        System.out.println("service method");
+//        super.service(req, res);
+//    }
 }
