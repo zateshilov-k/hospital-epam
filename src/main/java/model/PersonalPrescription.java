82647250b1be0b7946b3089edc5e120ea902716c
ColdAsIcee <Jurlique@mail.ru>
2018-12-26 22:26:06 +0300

    added PersonalPrescription

---
+++ src/main/java/model/PersonalPrescription.java
@@ -0,0 +1,24 @@
+package model;
+
+public class PersonalPrescription {
+
+    private long personalId;
+    private long prescriptionId;
+
+    public long getPersonalId() {
+        return personalId;
+    }
+
+    public void setPersonalId(long personalId) {
+        this.personalId = personalId;
+    }
+
+    public long getPrescriptionId() {
+        return prescriptionId;
+    }
+
+    public void setPrescriptionId(long prescriptionId) {
+        this.prescriptionId = prescriptionId;
+    }
+
+}
