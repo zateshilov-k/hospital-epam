0756748371cff0c2ca579061be98d5d5afbf6a8a
ColdAsIcee <Jurlique@mail.ru>
2018-12-26 22:08:58 +0300

    patient done

--- src/main/java/model/Patient.java
+++ src/main/java/model/Patient.java
@@ -2,25 +2,25 @@ package model;
 
 public class Patient {
 
-    private int id;
-    private String name;
+    private int patientId;
+    private String firstname;
     private String surname;
     private boolean isDischarged;
 
-    public int getId() {
-        return id;
+    public int getPatientId() {
+        return patientId;
     }
 
-    public void setId(int id) {
-        this.id = id;
+    public void setPatientId(int patientId) {
+        this.patientId = patientId;
     }
 
-    public String getName() {
-        return name;
+    public String getFirstname() {
+        return firstname;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
     }
 
     public String getSurname() {
