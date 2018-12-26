b88c4e9fc829c5ecbc5c820c8f19b9071e48abca
Alexey <taler2002@gmail.com>
2018-12-26 14:21:58 +0300

    Обработка страницы логина

--- src/main/java/services/LoginValidate.java
+++ src/main/java/services/LoginValidate.java
@@ -2,9 +2,8 @@ package services;
 
 public class LoginValidate {
 
-    public boolean authenticate (String login, String password){
-
-        if (password == null || password.trim()=="")         return false;
+    public boolean authenticate(String login, String password) {
+        if (login == null || login.equals("") || password == null || password.trim() == "") return false;
         return true;
     }
 }
