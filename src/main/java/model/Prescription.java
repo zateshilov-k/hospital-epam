cde80678a0586f0ccbeeeb7a8b0504444df33376
ColdAsIcee <Jurlique@mail.ru>
2018-12-26 22:24:41 +0300

    updated Prescription

--- src/main/java/model/Prescription.java
+++ src/main/java/model/Prescription.java
@@ -2,10 +2,59 @@ package model;
 
 public class Prescription {
 
-    private int id;
+    private long prescriptionId;
     private String description;
-    private Role role;
-    private Personal personal;
-    private Patient patient;
+    private long personalId;
+    private long patientId;
+    private long diagnosisId;
+    private boolean isDone;
+
+    public long getPrescriptionId() {
+        return prescriptionId;
+    }
+
+    public void setPrescriptionId(long prescriptionId) {
+        this.prescriptionId = prescriptionId;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public long getPersonalId() {
+        return personalId;
+    }
+
+    public void setPersonalId(long personalId) {
+        this.personalId = personalId;
+    }
+
+    public long getPatientId() {
+        return patientId;
+    }
+
+    public void setPatientId(long patientId) {
+        this.patientId = patientId;
+    }
+
+    public long getDiagnosisId() {
+        return diagnosisId;
+    }
+
+    public void setDiagnosisId(long diagnosisId) {
+        this.diagnosisId = diagnosisId;
+    }
+
+    public boolean isDone() {
+        return isDone;
+    }
+
+    public void setDone(boolean done) {
+        isDone = done;
+    }
 
 }
