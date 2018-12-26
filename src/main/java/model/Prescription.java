27f59a632f7ca38620a3d21ae28d5b9e9e669763
ColdAsIcee <Vladislav_Rospuskov@epam.com>
2018-12-26 12:21:35 +0300

    writing model

---
+++ src/main/java/model/Prescription.java
@@ -0,0 +1,11 @@
+package model;
+
+public class Prescription {
+
+    private int id;
+    private String description;
+    private Role role;
+    private Personal personal;
+    private Patient patient;
+
+}
