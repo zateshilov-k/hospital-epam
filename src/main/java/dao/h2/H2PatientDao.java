7488fb9872c4f5ea84e3bbfca3bb34e72b838681
ColdAsIcee <Vladislav_Rospuskov@epam.com>
2018-12-29 14:38:14 +0300

    added patientDao - works. Class ListOfPatients for testing.

--- src/main/java/dao/h2/H2PatientDao.java
+++ src/main/java/dao/h2/H2PatientDao.java
@@ -11,19 +11,21 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.Logger;
 
 public class H2PatientDao implements PatientDao {
 
     @Resource(name = "jdbc/hospital-h2-db")
     private DataSource dataSource;
+    private static final Logger log = Logger.getLogger(String.valueOf(H2PatientDao.class));
 
     public H2PatientDao(DataSource dataSource) {
         this.dataSource = dataSource;
     }
 
     // SQL queries
-    private static final String GET_ALL_PATIENTS_SQL = "SELECT patient_id, first_name, last_name, is_discharged FROM "
-            + "patient WHERE patient_id = ?";
+    private static final String GET_ALL_PATIENTS_SQL = "SELECT patient_id, first_name, last_name, is_discharged FROM " +
+            "patient";
 
     @Override
     public List<Patient> getAllPatients() {
@@ -33,21 +35,15 @@ public class H2PatientDao implements PatientDao {
             try (ResultSet resultSet = statement.executeQuery()) {
                 while (resultSet.next()) {
                     Patient patient = new Patient();
-                    System.out.println(1);
                     patient.setPatientId(resultSet.getLong("patient_id"));
-                    System.out.println(2);
                     patient.setFirstName(resultSet.getString("first_name"));
-                    System.out.println(3);
                     patient.setLastName(resultSet.getString("last_name"));
-                    System.out.println(4);
                     patient.setDischarged(resultSet.getBoolean("is_discharged"));
-                    System.out.println(5);
                     patients.add(patient);
-                    System.out.println(6);
                 }
             }
         } catch (SQLException e) {
-            System.err.println("Wrong add patient in patients list");
+            log.warning(e.getMessage());
         }
         return patients;
     }
