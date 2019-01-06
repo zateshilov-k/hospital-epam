faa55bfd771cce8438a0e81c9dae8550b1c104a6
Alexey <taler2002@gmail.com>
2019-01-06 13:46:37 +0300

    added PersonalList servlet

--- src/main/java/controllers/PersonalList.java
+++ src/main/java/controllers/PersonalList.java
@@ -1,22 +1,37 @@
 package controllers;
 
+import dao.DaoFactory;
+import dao.PersonalDao;
+import model.Personal;
+import services.PersonalService;
+
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
+import java.util.List;
 
 @WebServlet("/personals")
 public class PersonalList extends HttpServlet {
+    DaoFactory daoFactory;
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         super.doGet(req, resp);
     }
 
     @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.doPost(req, resp);
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        HttpSession session = request.getSession(true);
+        response.setContentType("text/html;charset=utf-8");
+        List<Personal> personals = new PersonalService().getAllPersonals(daoFactory);
+        if (personals != null) {
+            session.setAttribute("personals", personals);
+        }
+        request.getRequestDispatcher("/personals.jsp").forward(request, response);
     }
 
     @Override
@@ -27,5 +42,7 @@ public class PersonalList extends HttpServlet {
     @Override
     public void init() throws ServletException {
         super.init();
+        ServletContext context = getServletContext();
+        daoFactory = (DaoFactory) context.getAttribute("daoFactory");
     }
 }
