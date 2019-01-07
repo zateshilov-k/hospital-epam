66b40b13d4cf7dd6ace743d9b6920f331cb21314
Alexey <taler2002@gmail.com>
2019-01-08 00:53:25 +0300

    some refactoring

--- src/main/java/controllers/AddPatientServlet.java
+++ src/main/java/controllers/AddPatientServlet.java
@@ -16,7 +16,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
-import javax.sql.DataSource;
 import java.io.IOException;
 import java.util.List;
 import java.util.Locale;
@@ -25,8 +24,7 @@ import java.util.ResourceBundle;
 сервлет, отрабатывающий желание добавить пациента
  */
 @WebServlet("/addPatient")
-public class PatientServlet extends HttpServlet {
-    DataSource dataSource;
+public class AddPatientServlet extends HttpServlet {
     DaoFactory daoFactory;
 
     @Override
@@ -36,7 +34,6 @@ public class PatientServlet extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-//        System.out.println("Patient Servlet doPost");
         HttpSession session = request.getSession(true);
         Locale locale = (Locale) session.getAttribute("locale");
         if (locale == null) {
@@ -46,7 +43,6 @@ public class PatientServlet extends HttpServlet {
         response.setContentType("text/html;charset=utf-8");
         String firstName = request.getParameter("firstName").trim();
         String lastName = request.getParameter("lastName").trim();
-        System.out.println("ФИО: "+ firstName + " " + lastName);
         StringFieldValidate stringFieldValidate = new StringFieldValidate();
         boolean isValid = stringFieldValidate.doValidation(firstName);
         if (isValid) {
